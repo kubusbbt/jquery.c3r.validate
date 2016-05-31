d80e899d4a7196c05a50585c2b9e2b639d4f3844
Kuba <kubusbbt@gmail.com>
2016-05-31 11:24:50 +0200

    dodana walidacja selecta dodana funkcja parent

--- js/walidacja.js
+++ js/walidacja.js
@@ -2,7 +2,7 @@
 /***************************************************
 
 telefon
-data-pattern="^[0-9+ ]{9,13}$"
+data-pattern="^[0-9+ ]{9,13}$" maxlength="13"
 
 data urodzenia
 data-pattern="^[0-9]{1,2}[-/.][0-9]{1,2}[-/.][0-9]{4}$"
@@ -18,138 +18,161 @@ data-pattern="^[0-9]{2}-[0-9]{3}$"
 
 $(document).ready(function(){
 
+	function parentElement( element ){
+		var level = $(element).parent();
+		return level;
+	}
+	function parentElementCheckbox( element ){
+		var level = $(element).parent().parent();
+		return level;
+	}
 
-function testInputText(element) {
-	if( $(element).attr('data-pattern') != null ){
+	function testInputText(element) {
+		if( $(element).attr('data-pattern') != null ){
+			var str = $(element).val();
+			var reg = new RegExp( $(element).attr('data-pattern'), 'gi' );
+
+		    if ( !reg.test(str) ) {
+		        parentElement(element).removeClass('valid').addClass('invalid');	        
+		        return false;
+		    } else {
+		        parentElement(element).addClass('valid').removeClass('invalid');
+		        return true;
+		    }	
+		}else{
+			if ( $(element).val() != '' ) {
+		        parentElement(element).addClass('valid').removeClass('invalid');
+		        return true;
+		    } else {
+		        parentElement(element).removeClass('valid').addClass('invalid');
+		        return false;
+		    }	
+		}
+	}
+
+	function testInputEmail(element) {
+		var pattern = /^[0-9a-zA-Z_.-]+@[0-9a-zA-Z.-]+\.[a-zA-Z]{2,3}$/;
 		var str = $(element).val();
-		var reg = new RegExp( $(element).attr('data-pattern'), 'gi' );
+		var reg = pattern;
 
 	    if ( !reg.test(str) ) {
-	        $(element).parent().removeClass('valid').addClass('invalid');
+	        parentElement(element).removeClass('valid').addClass('invalid');
 	        return false;
 	    } else {
-	        $(element).parent().addClass('valid').removeClass('invalid');
+	        parentElement(element).addClass('valid').removeClass('invalid');
 	        return true;
 	    }	
-	}else{
+	}
+
+	function testInputCheckbox(element){
+		if( element.checked == true ){
+		    parentElementCheckbox(element).addClass('valid').removeClass('invalid');
+		    return true;
+		}else{
+		    parentElementCheckbox(element).removeClass('valid').addClass('invalid');
+		    return false;		
+		}
+	}
+
+	function testInputSelect(element){
+		if( $(element).val() != '' ){
+		    parentElement(element).addClass('valid').removeClass('invalid');
+		    return true;
+		}else{
+		    parentElement(element).removeClass('valid').addClass('invalid');
+		    return false;		
+		}
+	}
+
+	function testTextarea(element){
 		if ( $(element).val() != '' ) {
-	        $(element).parent().addClass('valid').removeClass('invalid');
+	        parentElement(element).addClass('valid').removeClass('invalid');
 	        return true;
 	    } else {
-	        $(element).parent().removeClass('valid').addClass('invalid');
+	        parentElement(element).removeClass('valid').addClass('invalid');
 	        return false;
-	    }	
-	}
-}
-
-function testInputEmail(element) {
-	var pattern = /^[0-9a-zA-Z_.-]+@[0-9a-zA-Z.-]+\.[a-zA-Z]{2,3}$/;
-	var str = $(element).val();
-	var reg = pattern;
-
-    if ( !reg.test(str) ) {
-        $(element).parent().removeClass('valid').addClass('invalid');
-        return false;
-    } else {
-        $(element).parent().addClass('valid').removeClass('invalid');
-        return true;
-    }	
-}
-
-function testInputCheckbox(element){
-	if( element.checked == true ){
-	    $(element).parent().addClass('valid').removeClass('invalid');
-	    return true;
-	}else{
-	    $(element).parent().removeClass('valid').addClass('invalid');
-	    return false;		
-	}
-}
-
-function testTextarea(element){
-	if ( $(element).val() != '' ) {
-        $(element).parent().addClass('valid').removeClass('invalid');
-        return true;
-    } else {
-        $(element).parent().removeClass('valid').addClass('invalid');
-        return false;
-    }
-}
-
-
-
-//remove attr required
-$('input, textarea, select').each(function(){
-	if( $(this).attr('required') ){
-		$(this).removeAttr('required');
-		$(this).addClass('required');
-	}
-});
-
-
-//live walidation
-$(document).on('keyup blur change', 'input, textarea', function(event){
-	if( $(this).attr('type') == 'text' ){
-		testInputText(this);
+	    }
 	}
 
-	if( $(this).attr('type') == 'email' ){
-		testInputEmail(this);
-	}
-
-	if( $(this).attr('type') == 'checkbox' ){
-		testInputCheckbox(this);
-	}
-
-	if( $(this).is('textarea') ){
-		testTextarea(this);
-	}
-});
-
-
-//walidation on form send
-$('form').submit(function(){
-	output = true;
-
-	$('input[type=text].required').each(function(){
-		if( testInputText(this) == false ){
-			output = false;
+	//remove attr required
+	$('input, textarea, select').each(function(){
+		if( $(this).attr('required') ){
+			$(this).removeAttr('required');
+			$(this).addClass('required');
 		}
 	});
 
-	$('input[type=email].required').each(function(){
-		if( testInputEmail(this) == false ){
-			output = false;
+	//live walidation
+	$(document).on('keyup blur change', 'input, textarea, select', function(event){
+		if( $(this).attr('type') == 'text' ){
+			testInputText(this);
+		}
+
+		if( $(this).attr('type') == 'email' ){
+			testInputEmail(this);
+		}
+
+		if( $(this).attr('type') == 'checkbox' ){
+			testInputCheckbox(this);
+		}
+
+		if( $(this).is('select') ){
+			testInputSelect(this);
+		}
+
+		if( $(this).is('textarea') ){
+			testTextarea(this);
 		}
 	});
 
-	$('input[type=checkbox].required').each(function(){
-		if( testInputCheckbox(this) == false ){
-			output = false;
-		}
-	});
+	//walidation on form send
+	$('form').submit(function(){
+		output = true;
 
-	$('textarea.required').each(function(){
-		if( testTextarea(this) == false ){
-			output = false;
-		}
-	});
-	
-	if( output == true ){		
-		$.ajax({
-			url: 'ajax.php',
-			type: 'POST',
-			data: $(this).serialize(),
-
-			success: function(data){
-			  $('#ajax').html(data);
+		$('input[type=text].required').each(function(){
+			if( testInputText(this) == false ){
+				output = false;
 			}
 		});
-	}
 
-	return false;
-	// return output;
-});
+		$('input[type=email].required').each(function(){
+			if( testInputEmail(this) == false ){
+				output = false;
+			}
+		});
 
+		$('input[type=checkbox].required').each(function(){
+			if( testInputCheckbox(this) == false ){
+				output = false;
+			}
+		});
+
+		$('select.required').each(function(){
+			if( testInputSelect(this) == false ){
+				output = false;
+			}
+		});
+
+		$('textarea.required').each(function(){
+			if( testTextarea(this) == false ){
+				output = false;
+			}
+		});
+		
+		if( output == true ){		
+			$.ajax({
+				url: 'ajax.php',
+				type: 'POST',
+				data: $(this).serialize(),
+
+				success: function(data){
+				  $('#ajax').html(data);
+				}
+			});
+		}
+
+		return false;
+		// return output;
+	});
 
 }); //end document ready
